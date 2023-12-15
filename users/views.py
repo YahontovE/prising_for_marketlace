be7069fcf65453a1fb127fe0b3923508ec88893d
Evgen <yahontov-sport@inbox.ru>
2023-12-15 15:43:48 +0300

    add cod

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.models import User
+from users.permissions import IsSuperuser
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    """Эндпоинт создания(регистрация) юзера"""
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class UserListAPIView(generics.ListAPIView):
+    """Эндпоинт просмотра всех юзеров"""
+    serializer_class = UserSerializer
+    permission_classes =[IsSuperuser]
+    queryset = User.objects.all()
